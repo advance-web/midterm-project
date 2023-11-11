63bd81a0bf0a7dc7c7f53e4a8aa8df4284b6fdc2
tkhoang1307 <tkhoangcr.fit.hcmus@gmail.com>
2023-11-11 21:46:37 +0700

    complete part: signin, signup, landing page

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -1,5 +1,29 @@
-const Landing = () => {
-  return <div>Landing</div>;
-};
+import PersonalCardInfo from "../personalCardInfo";
 
-export default Landing;
+import "../../css/landingStyle.css"
+
+const listPersonalInfo = [{
+  studentID: '20120290',
+  studentName: 'Trần Khánh Hoàng',
+  dateOfBirth: '13/07/2002',
+  major: 'Kĩ thuật phần mềm'
+},
+{
+  studentID: '20120291',
+  studentName: 'Trần Khánh Hoàng',
+  dateOfBirth: '13/07/2002',
+  major: 'Kĩ thuật phần mềm'
+}];
+
+export default function Landing() {
+  return (
+    <div className="listCardInfo">
+      {listPersonalInfo.map((personalInfo) =>
+        <div key={personalInfo.studentID} className='itemCardInfo'>
+          <PersonalCardInfo data={personalInfo}/>
+        </div>
+      )}
+
+    </div>
+  );
+}
--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -1,13 +1,68 @@
-import { Input } from "antd";
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input } from 'antd';
 
-const SignIn = () => {
+import '../../css/signinStyle.css'
+
+export default function SignIn() {
+
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
   return (
-    <div>
-      <h1>Sign in Form</h1>
-      <Input placeholder="Tài khoản" />
-      <Input placeholder="Mật khẩu" />
+    <div className="signin">
+      <div className="title-signin">
+        Đăng nhập
+      </div>
+      <Form name="normal_signin" className="signin-form"
+        labelCol={{ span: 8, }}
+        wrapperCol={{ span: 16, }}
+        style={{ maxWidth: 600, }}
+        initialValues={{ remember: true, }}
+        onFinish={onFinish}
+      >
+        <Form.Item label="Username" name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Username!',
+            },
+          ]}
+        >
+          <Input size="large" prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username" />
+        </Form.Item>
+
+        <Form.Item label="Password" name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Password!',
+            },
+          ]}
+        >
+          <Input size="large" prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password" placeholder="Password" />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16, }}>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className="signin-form-forgot" href="# ">
+            Forgot password
+          </a>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16, }}>
+          <Button type="primary" htmlType="submit" className="signin-form-button">
+            Log in
+          </Button>
+          Or <a href="# ">register now!</a>
+        </Form.Item>
+      </Form>
     </div>
   );
-};
 
-export default SignIn;
+}
+
