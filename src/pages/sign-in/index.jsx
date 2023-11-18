f10d31c6cc5e4ab0abccccf6ad299ae90dffef12
tkhoang1307 <tkhoangcr.fit.hcmus@gmail.com>
2023-11-18 13:29:08 +0700

    add footer

--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -1,73 +1,55 @@
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
 
-<<<<<<< HEAD
+import { signIn } from "../../services/auth"
+
 import '../../css/signinStyle.css'
 
 export default function SignIn() {
 
-=======
-import '../../css/signinStyle.css';
-
-export default function SignIn() {
->>>>>>> c10ef74a5324f9204c3006be7ba65a44d8a74b51
   const onFinish = (values) => {
     console.log('Received values of form: ', values);
   };
+
+  const handleSignIn = () => {
+    const data = form.getFieldValue();
+    const dataCallAPI = {
+        "email": data.email,
+        "password": data.password
+    }
+    console.log("AloAlo123: ", dataCallAPI);
+    const dataReturn = signIn(dataCallAPI);
+    console.log("API Response: ", dataReturn);
+    form.submit();
+};
+
+const [form] = Form.useForm();
+
   return (
     <div className="signin">
-<<<<<<< HEAD
       <div className="title-signin">
         Đăng nhập
       </div>
-      <Form name="normal_signin" className="signin-form"
+      <Form form={form} name="normal_signin" className="signin-form"
         labelCol={{ span: 8, }}
         wrapperCol={{ span: 16, }}
         style={{ maxWidth: 600, }}
         initialValues={{ remember: true, }}
         onFinish={onFinish}
       >
-        <Form.Item label="Username" name="username"
-=======
-      <div className="title-signin">Đăng nhập</div>
-      <Form
-        name="normal_signin"
-        className="signin-form"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          label="Username"
-          name="username"
->>>>>>> c10ef74a5324f9204c3006be7ba65a44d8a74b51
+        <Form.Item label="Email" name="email"
           rules={[
             {
               required: true,
-              message: 'Please input your Username!',
+              message: 'Please input your email!',
             },
           ]}
         >
-<<<<<<< HEAD
           <Input size="large" prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Username" />
+            placeholder="Email" />
         </Form.Item>
 
         <Form.Item label="Password" name="password"
-=======
-          <Input
-            size="large"
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Username"
-          />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
->>>>>>> c10ef74a5324f9204c3006be7ba65a44d8a74b51
           rules={[
             {
               required: true,
@@ -75,23 +57,11 @@ export default function SignIn() {
             },
           ]}
         >
-<<<<<<< HEAD
           <Input size="large" prefix={<LockOutlined className="site-form-item-icon" />}
             type="password" placeholder="Password" />
         </Form.Item>
 
         <Form.Item wrapperCol={{ offset: 8, span: 16, }}>
-=======
-          <Input
-            size="large"
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
->>>>>>> c10ef74a5324f9204c3006be7ba65a44d8a74b51
           <Form.Item name="remember" valuePropName="checked" noStyle>
             <Checkbox>Remember me</Checkbox>
           </Form.Item>
@@ -101,17 +71,8 @@ export default function SignIn() {
           </a>
         </Form.Item>
 
-<<<<<<< HEAD
         <Form.Item wrapperCol={{ offset: 8, span: 16, }}>
-          <Button type="primary" htmlType="submit" className="signin-form-button">
-=======
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="signin-form-button"
-          >
->>>>>>> c10ef74a5324f9204c3006be7ba65a44d8a74b51
+          <Button type="primary" htmlType="submit" className="signin-form-button" onClick={handleSignIn}>
             Log in
           </Button>
           Or <a href="# ">register now!</a>
@@ -119,10 +80,5 @@ export default function SignIn() {
       </Form>
     </div>
   );
-<<<<<<< HEAD
-
 }
 
-=======
-}
->>>>>>> c10ef74a5324f9204c3006be7ba65a44d8a74b51
--- src/pages/sign-up/index.jsx
+++ src/pages/sign-up/index.jsx
@@ -1,6 +1,9 @@
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Form, Input } from 'antd';
-<<<<<<< HEAD
+
+import { signUp } from "../../services/auth"
+
+import React from 'react';
 
 import '../../css/signupStyle.css'
 
@@ -9,12 +12,31 @@ export default function SignUp() {
     const onFinish = (values) => {
         console.log('Received values of form: ', values);
     };
+
+    const handleSignUp = () => {
+        const data = form.getFieldValue();
+        const dataCallAPI = {
+            "name": data.fullname,
+            "email": data.email,
+            "password": data.password,
+            "confirmPassword": data.confirmPassword,
+            "phone": data.telephone,
+            "address": data.address
+        }
+        console.log("AloAlo123: ", dataCallAPI);
+        const dataReturn = signUp(dataCallAPI);
+        console.log("API Response: ", dataReturn);
+        form.submit();
+    };
+
+    const [form] = Form.useForm();
+
     return (
         <div className="signup">
             <div className="title-signup">
                 Đăng ký
             </div>
-            <Form name="normal_signup" className="signup-form"
+            <Form form={form} name="normal_signup" className="signup-form"
                 labelCol={{ span: 8, }}
                 wrapperCol={{ span: 16, }}
                 style={{ maxWidth: 600, }}
@@ -40,19 +62,30 @@ export default function SignUp() {
                         },
                     ]}
                 >
-                    <Input size="large" placeholder="Telephone" type="tel"/>
+                    <Input size="large" placeholder="Telephone" type="tel" />
                 </Form.Item>
 
-                <Form.Item label="Username" name="username"
+                <Form.Item label="Address" name="address"
                     rules={[
                         {
                             required: true,
-                            message: 'Please input your Username!',
+                            message: 'Please input your address!',
+                        },
+                    ]}
+                >
+                    <Input size="large" placeholder="Address" />
+                </Form.Item>
+
+                <Form.Item label="Email" name="email"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your email!',
                         },
                     ]}
                 >
                     <Input size="large" prefix={<UserOutlined className="site-form-item-icon" />}
-                        placeholder="Username" />
+                        placeholder="Email" />
                 </Form.Item>
 
                 <Form.Item label="Password" name="password"
@@ -73,14 +106,22 @@ export default function SignUp() {
                             required: true,
                             message: 'Please input your Confirm Password!',
                         },
+                        {
+                            validator: (_, value) => {
+                                if (!value || form.getFieldValue('password') === value) {
+                                    return Promise.resolve();
+                                }
+                                return Promise.reject(new Error('The two passwords do not match!'));
+                            },
+                        },
                     ]}
                 >
                     <Input size="large" prefix={<LockOutlined className="site-form-item-icon" />}
-                        type="confirmPassword" placeholder="Confirm Password" />
+                        type="password" placeholder="Confirm Password" />
                 </Form.Item>
 
                 <Form.Item wrapperCol={{ offset: 8, span: 16, }}>
-                    <Button type="primary" htmlType="submit" className="signup-form-button">
+                    <Button type="primary" htmlType="submit" className="signup-form-button" onClick={handleSignUp}>
                         Sign up
                     </Button>
                     Or <a href="# ">log in now!</a>
@@ -91,117 +132,3 @@ export default function SignUp() {
 
 }
 
-=======
-
-import '../../css/signupStyle.css';
-
-export default function SignUp() {
-  const onFinish = (values) => {
-    console.log('Received values of form: ', values);
-  };
-  return (
-    <div className="signup">
-      <div className="title-signup">Đăng ký</div>
-      <Form
-        name="normal_signup"
-        className="signup-form"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          label="Fullname"
-          name="fullname"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your fullname!',
-            },
-          ]}
-        >
-          <Input size="large" placeholder="Fullname" />
-        </Form.Item>
-
-        <Form.Item
-          label="Telephone"
-          name="telephone"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your telephone!',
-            },
-          ]}
-        >
-          <Input size="large" placeholder="Telephone" type="tel" />
-        </Form.Item>
-
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your Username!',
-            },
-          ]}
-        >
-          <Input
-            size="large"
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Username"
-          />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your Password!',
-            },
-          ]}
-        >
-          <Input
-            size="large"
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-
-        <Form.Item
-          label="Confirm Password"
-          name="confirmPassword"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your Confirm Password!',
-            },
-          ]}
-        >
-          <Input
-            size="large"
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="confirmPassword"
-            placeholder="Confirm Password"
-          />
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="signup-form-button"
-          >
-            Sign up
-          </Button>
-          Or <a href="# ">log in now!</a>
-        </Form.Item>
-      </Form>
-    </div>
-  );
-}
->>>>>>> c10ef74a5324f9204c3006be7ba65a44d8a74b51
